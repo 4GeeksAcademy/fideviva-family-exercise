c8be219b9b68b36584bf58324b04e11e2c016a09
José Fidel Paredes Sanchez <98322383+fideviva@users.noreply.github.com>
2024-06-05 01:49:01 +0000

    finished delete method

--- src/datastructures.py
+++ src/datastructures.py
@@ -11,23 +11,54 @@ from random import randint
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-        self._members = [] # Example list of members
+        self._members = [{'id': 10,
+                          'first_name': 'John',
+                          'last_name': self.last_name,
+                          'age': 33,
+                          'lucky_numbers': [7, 13, 22]},
+                         {'id': self._generate_id(),
+                          'first_name': 'Jane',
+                          'last_name': self.last_name,
+                          'age': 35,
+                          'lucky_numbers': [10, 14, 3]},
+                         {'id': self._generate_id(),
+                          'first_name': 'Jimmy',
+                          'last_name': self.last_name,
+                          'age': 5,
+                          'lucky_numbers': [1]}] # Example list of members
 
-    def _generateId(self):
+    def _generate_id(self):
         # Read-only: Use this method to generate random members ID's when adding members into the list
         return randint(0, 99999999)
 
     def add_member(self, member):
         # Fill this method and update the return
-        pass
+        member['id'] = self._generate_id()
+        member['last_name'] = self.last_name
+        print(member)
+        self._members.append(member)
+        return self._members
 
     def delete_member(self, id):
+        result = [self._members.remove(row) for row in self._members if row['id'] == id]
+        return result if result else None
         # Fill this method and update the return
-        pass
+        # Buscar al elemento que sea igual al row['id']
+        # Si lo encuentra lo elimina
+        # del self._members
 
     def get_member(self, id):
-        # Fill this method and update the return
-        pass
+        # Opcion 1 - for in
+        """
+        for row in self._members:
+            if row['id'] == id:
+                return row
+        """
+        # Opcion 2 - List comprension
+        result = [row for row in self._members if row['id'] == id]
+        return result[0] if result else None   # It will return only the first instance that contains the id. So if there is two instance with the same id, it will return the first.
+        
+            
 
     def get_all_members(self):
         # This method is done, it returns a list with all the family members
--- src/utils.py
+++ src/utils.py
@@ -26,18 +26,18 @@ def has_no_empty_params(rule):
 def generate_sitemap(app):
     links = []
     for rule in app.url_map.iter_rules():
-        # Filter out rules we can't navigate to in a browser
-        # and rules that require parameters
+        # Filter out rules we can't navigate to in a browser and rules that require parameters
         if "GET" in rule.methods and has_no_empty_params(rule):
             url = url_for(rule.endpoint, **(rule.defaults or {}))
             links.append(url)
-
-    links_html = "".join(["<li><a href='" + y + "'>" + y + "</a></li>" for y in links])
+    links_html = "".join(["<li><a href='" + link + "'>" + link + "</a></li>" for link in links])
     return """
         <div style="text-align: center;">
             <img src='https://github.com/breatheco-de/exercise-family-static-api/blob/master/rigo-baby.jpeg?raw=true' />
             <h1>Hello Rigo!!</h1>
             This is your api home, remember to specify a real endpoint path like: 
-            <ul style="text-align: left;">"""+links_html+ """
+            <ul style="text-align: left;">
+                """+links_html+ """
             </ul>
-        </div>"""
+        </div>
+        """
